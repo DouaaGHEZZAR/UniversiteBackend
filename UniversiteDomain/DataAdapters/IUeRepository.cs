2b19fcc80d0b2ee547030a387def5be1d3e02133
GHEZZAR Douaa <douaa.ghezzar@etud.u-picardie.fr>
2024-12-04 15:26:43 +0100

    Test unitaire with succes 8.3 chapitre 3 (fixing the is null and any() error with { Count: > 0 } now i'm starting the 8.4 part on chaptre 3

--- UniversiteDomain/DataAdapters/IUeRepository.cs
+++ UniversiteDomain/DataAdapters/IUeRepository.cs
@@ -1,7 +1,8 @@
 using UniversiteDomain.Entities;
+
 namespace UniversiteDomain.DataAdapters;
 
-public interface IEtudiantRepository : IRepository<Etudiant>
+public interface IUeRepository : IRepository<Ue>
 {
-   
+    
 }
--- UniversiteDomain/UseCases/ParcoursUseCases/Create/CreateParcoursUseCase.cs
+++ UniversiteDomain/UseCases/ParcoursUseCases/Create/CreateParcoursUseCase.cs
@@ -1,11 +1,12 @@
 using UniversiteDomain.DataAdapters;
+using UniversiteDomain.DataAdapters.DataAdaptersFactory;
 using UniversiteDomain.Entities;
 using UniversiteDomain.Exceptions.ParcoursException;
 using UniversiteDomain.Util;
 
 namespace UniversiteDomain.UseCases.ParcoursUseCases.Create;
 
-public class CreateParcoursUseCase(IParcoursRepository parcoursRepository)
+public class CreateParcoursUseCase(IRepositoryFactory repositoryFactory)
 {
     public async Task<Parcours> ExecuteAsync(long Id, string NomParcours, int AnneeFormaion)
     {
@@ -15,8 +16,8 @@ public class CreateParcoursUseCase(IParcoursRepository parcoursRepository)
     public async Task<Parcours> ExecuteAsync(Parcours parcours)
     {
         await CheckBusinessRules(parcours);
-        Parcours pr = await parcoursRepository.CreateAsync(parcours);
-        parcoursRepository.SaveChangesAsync().Wait();
+        Parcours pr = await repositoryFactory.ParcoursRepository().CreateAsync(parcours);
+        repositoryFactory.ParcoursRepository().SaveChangesAsync().Wait();
         return pr;
     }
     
@@ -26,17 +27,17 @@ public class CreateParcoursUseCase(IParcoursRepository parcoursRepository)
         ArgumentNullException.ThrowIfNull(parcours.Id);
         ArgumentNullException.ThrowIfNull(parcours.NomParcours);
         ArgumentNullException.ThrowIfNull(parcours.AnneeFormation);
-        ArgumentNullException.ThrowIfNull(parcoursRepository);
+        ArgumentNullException.ThrowIfNull(repositoryFactory.ParcoursRepository());
         
         // On recherche un parcours avec le même Id parcours
-        List<Parcours> existe = await parcoursRepository.FindByConditionAsync(p=>p.Id.Equals(parcours.Id));
+        List<Parcours> existe = await repositoryFactory.ParcoursRepository().FindByConditionAsync(p=>p.Id.Equals(parcours.Id));
 
         // Si un parcours avec le même Id existe déjà, on lève une exception personnalisée
-        if (existe .Any()) throw new DuplicateIdException(parcours.Id+ " - cet Id est déjà affecté à un parcours");
+        if (existe is {Count:>0}) throw new DuplicateIdException(parcours.Id+ " - cet Id est déjà affecté à un parcours");
         
         
         // On vérifie si l'email est déjà utilisé
-        existe = await parcoursRepository.FindByConditionAsync(p=>p.NomParcours.Equals(parcours.NomParcours));
+        existe = await repositoryFactory.ParcoursRepository().FindByConditionAsync(p=>p.NomParcours.Equals(parcours.NomParcours));
         // Une autre façon de tester la vacuité de la liste
         if (existe is {Count:>0}) throw new DuplicateNomParcoursException(parcours.NomParcours +" est déjà affecté à un parcours");
         
